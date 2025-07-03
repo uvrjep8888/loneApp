94cf881521207b8c31c45916ecbee11f8ba532bd
Jeffrey Uvero <136442534+uvrjep8888@users.noreply.github.com>
2025-07-03 16:52:54 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('🦁 Hello Chief! Your API is live and Dockerized!');
+});
+
+app.get('/ping', (req, res) => {
+  res.json({ message: 'pong 🏓' });
+});
+
+app.listen(PORT, () => {
+  console.log(`API running on http://localhost:${PORT}`);
+});
